79bd1915280107e89e4a6b72953b3f6810511c6c
Bilques <bilques.jan13@qmail.cuny.edu>
2023-11-27 19:01:11 -0500

    User

--- untitled/src/User.java
+++ untitled/src/User.java
@@ -7,6 +7,14 @@ public class User {
 
     ArrayList<Patient> patients;
 
+    public User (int UID, String UName, String password) {
+        this.userID=UID;
+        this.userName = UName;
+        this.password=password;
+        this.patients = new ArrayList<>();
+    }
+
+
     Patient SearchForPatient(Patient patient)
     {
         return null;
@@ -23,14 +31,48 @@ public class User {
     }
 
     void FillInNewPatientInfo(Patient patient, Info info)
-    {
+    { 
+        if (patient != null && info != null) {
+            patient.setInfo(info);
+            System.out.println("Patient information filled in successfully.");
+        } else {
+            System.out.println("Invalid patient information ");
+        }
+
 
     }
 
-    void ShowPrediction(Patient patient)
-    {
+    public static boolean showPrediction (Patient patient) {
+        boolean prediction = patient.predictDiseaseProgression();
 
+        if (!prediction){
+        System.out.println("Prediction for Patient " + patient.getPatientID() + " using RandomForest is  Bad \n");
+        return false;
+        }
+        else {
+            System.out.println("Prediction for Patient " + patient.getPatientID() + " using RandomForest is Good \n");
+            return true;
+        }
     }
+
+
+    public String getUserName () {
+        return userName;
+    }
+
+    public String getPassword () {
+        return password;
+    }
+
+    public void addPatient(Patient patient) {
+        patients.add(patient);
+    }
+
+
+    public ArrayList<Patient> getPatients() {
+        return patients;
+    }
+
 
 
 
